9e1b069bd8fbbae616427ee0f1647743c29a59e0
kenwizi <kenwizi@hotmail.com>
2018-12-03 16:06:34 -0700

    app setup

---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+ class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <h1>course scheduler</h1>
+       
+      </div>
+    );
+  }
+}
+
+
+export default Home;
